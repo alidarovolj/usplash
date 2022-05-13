dee6701952be15f20b626db8f64e87414585b9e6
Olzhas <oljasalidarov@gmail.com>
2022-05-13 21:55:02 +0600

    usplash

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container mx-auto">
+      <div class="flex flex-wrap justify-between">
+        <div class="w-1/5" v-for="photo of photos.results" :key="photo.id">
+          <img class="w-full h-full object-cover" :src="photo.urls.regular" alt="" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  data() {
+    return {
+      photos: [],
+    };
+  },
+  async mounted() {
+    let res = await axios.get(
+      "https://api.unsplash.com/search/photos?query={'dogs'}&client_id=UxQcyiv7djwaIznmB6G73bYjldVCcICcUT4HZpscGcI"
+    );
+    this.photos = res.data;
+  },
+};
 </script>
